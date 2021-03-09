bf0a796e34a722d5d2789812f25acbccf37d1615
Maielo <maielo.mv@gmail.com>
2021-03-09 01:26:29 +0100

    complete refactor to TS with new API

---
+++ README.md
@@ -0,0 +1,90 @@
+- fills special characters in native language (except Asian, Arabian) ✔️
+- fills multiple form fields using same acro name ✔️
+- handles `undefined` and `null` values with empty string ✔️
+- all asynchronous to not blocking main thread ✔️
+- return `Buffer` ✔️
+- handles checkboxes ✔️
+- lightweight and no dependencies ✔️
+- exposes `pdftk` and `xfdf` for customization ✔️
+- written in typescript ✔️
+
+## Usage
+
+```
+npm i pdftk-fill-pdf
+```
+
+#### basic usage
+
+```js
+// commonJS
+const pdfFill = require('pdftk-fill-pdf').default;
+// ES6 modules
+import pdfFill from 'pdftk-fill-pdf';
+
+// key is fieldname in PDF
+const fillData = {
+  test1: 'abcdefghchijklmnopqrstuvwxyz123456',
+  test2: 'another text',
+  test_number: 123456789,
+  test_special_chars:
+    'ěščřžýáíéúůťńóú жжжююю ć, ń, ó, ś, ź Ä/ä, Ö/ö, Ü/ü  /{}"?>:{',
+  test_checkbox: true,
+  test_radio: true,
+  test_null: null,
+  test_undefined: undefined,
+};
+
+// your code
+try {
+  const pdfBuffer = await pdfFill.fill({
+    pdfPath: 'test/test.pdf',
+    data: fillData,
+  });
+
+  // do with buffer whatever you want i.e. write it into file
+  fs.writeFileSync('./test/test_result.pdf', pdfBuffer, { encoding: 'utf8' });
+} catch (err) {
+  console.error(err);
+}
+```
+
+#### Generate xfdf
+
+```js
+const fillData = {
+  test1: 'abcdefghchijklmnopqrstuvwxyz123456',
+  test2: 'another text',
+  test_number: 123456789,
+};
+
+try {
+  const xfdfString = await pdfFill.generateXfdf({
+    pdfPath: 'test/test.pdf', // used for correct Xfdf href
+    data: fillData, // same data as above
+  });
+
+  console.log(xfdfString);
+} catch (err) {
+  console.error(err);
+}
+```
+
+#### Your own pdftk execution
+
+docs: [pdftk the pdf toolkit](https://www.pdflabs.com/tools/pdftk-the-pdf-toolkit/)
+
+```js
+try {
+  // ...
+  // node.js exec options
+  const OPTIONS = {
+    timeout: 100000,
+    maxBuffer: 200 * 1024,
+  };
+  const cmd = `fill_form ${xfdfFile} output ${generatedPdfFile} need_appearances`;
+  const stdout = await pdfFill.generateXfdf(cmd, OPTIONS);
+} catch (err) {
+  console.log(err);
+}
+```
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,120 @@
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
-  }
-  return a + b;
+import fs from 'fs';
+import path from 'path';
+import { exec, ExecOptions } from 'child_process';
+import { promises as fsp } from 'fs';
+
+export type FillData = Record<string, string | number | boolean>;
+
+const TEMP = path.resolve('./temp');
+
+function _getRandomName() {
+  return `file_${Math.round(Math.random() * 10000)}`;
+}
+
+const pdfFill = {
+  fill: async function({
+    data,
+    pdfPath,
+  }: {
+    data: FillData;
+    pdfPath: string;
+  }): Promise<Buffer> {
+    if (!data || typeof data !== 'object') {
+      throw new Error(
+        'pdf-fill ~ fillData is not defined (should be object of data to fill)'
+      );
+    }
+
+    if (!pdfPath || typeof pdfPath !== 'string') {
+      throw new Error('pdf-fill ~ pdfPath is not defined');
+    }
+    const _pdfPath = path.resolve(pdfPath);
+
+    if (typeof pdfPath === 'string' && !fs.existsSync(_pdfPath)) {
+      throw new Error('pdf-fill ~ there is not file : ' + pdfPath);
+    }
+    const _xfdf = await this.generateXfdf({ data, pdfPath });
+
+    const bulkRandomName = _getRandomName();
+    const xfdfFile = `${TEMP}${path.sep}${bulkRandomName}.xfdf`;
+    const generatedPdfFile = `${TEMP}${path.sep}${bulkRandomName}.pdf`;
+
+    // write xdfd for pdftk
+    await fsp.writeFile(xfdfFile, _xfdf);
+    // run pdftk (it returns shit...only on error)
+    await this.execPdftk(
+      `${_pdfPath} fill_form ${xfdfFile} output ${generatedPdfFile} need_appearances`
+    );
+
+    const file = await fsp.readFile(generatedPdfFile);
+    // cleanup
+    await fsp.unlink(xfdfFile);
+    await fsp.unlink(generatedPdfFile);
+
+    return file;
+  },
+  generateXfdf: async function({
+    data,
+    pdfPath,
+  }: {
+    data: FillData;
+    pdfPath: string;
+  }): Promise<string> {
+    const _pdfPath = path.resolve(pdfPath);
+
+    return `
+
+    <?xml version="1.0" encoding="UTF-8"?>
+      <xfdf xmlns="http://ns.adobe.com/xfdf/" xml:space="preserve">
+        <f href="${_pdfPath}"/>
+        <fields>
+          ${Object.keys(data)
+            .map(key => {
+              let value = data[key];
+
+              if (value === null || value === undefined) {
+                value = '';
+              }
+
+              if (typeof value === 'boolean') {
+                value = value ? 'Yes' : 'Off';
+              }
+
+              return `
+            <field name="${key}">
+              <value>${value}</value>
+            </field>
+            `;
+            })
+            .join('')}
+        </fields>
+      </xfdf>
+    `;
+  },
+  execPdftk(cmd: string, customExecOptions?: ExecOptions): Promise<string> {
+    const OPTIONS = {
+      timeout: 100000,
+      maxBuffer: 200 * 1024,
+      ...customExecOptions,
+    };
+    console.log('cmd', cmd);
+    return new Promise((resolve, reject) => {
+      exec(`pdftk ${cmd}`, OPTIONS, function(error, stdout, stderr) {
+        console.log(stdout);
+        if (error) {
+          console.error(error);
+          return reject(error);
+        }
+
+        if (stderr) {
+          console.error(stderr);
+          return reject(stderr);
+        }
+
+        return resolve(stdout);
+      });
+    });
+  },
 };
+
+export default pdfFill;
